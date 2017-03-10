fd7dc809930eea9d3e6aa66a9a32dd2c4ec39e90
Atsushi <atsushi88885@gmail.com>
2017-03-10 23:05:42 +0900

    lesson15終了

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-require('conection.php');
+require('connection.php');
 
 function create($data) {
   insertDb($data['todo']);
@@ -8,4 +8,28 @@ function create($data) {
 //全件取得
 function index() {
   return $todos = selectAll();
+}
+
+//更新
+function update($data) {
+  updateDb($data['id'], $data['todo']);
+}
+
+//詳細の取得
+function detail($id) {
+  return getSelectData($id);
+}
+
+function checkReferer() {
+  $httpArr = parse_url($_SERVER['HTTP_REFERER']);
+  return $res = transition($httpArr['path']);
+}
+
+function transition($path) {
+  $data = $_POST;
+  if($path === '/new.php') {
+    create($data);
+  }elseif($path ==='/edit.php') {
+    update($data);
+  }
 }
